5c10ba8c2aa857f3fa883a10668461cbf114ed79
James Thistlewood <jamesthistlewood@gmail.com>
2018-10-12 18:58:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+
+import pygame, math, sys
+from colours import *
+from objects import Vector, Ball, Point
+
+
+def main():
+	pygame.init()
+	SCREEN_WIDTH = 800
+	SCREEN_HEIGHT = 600
+	SCALE = 80	# pixels per meter
+	WIDTH = SCREEN_WIDTH/SCALE
+	HEIGHT = SCREEN_HEIGHT/SCALE
+
+	SCREEN_CENTRE = Point(WIDTH/2, HEIGHT/2)
+	CENTRE = SCREEN_CENTRE/SCALE
+
+	DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	CLOCK = pygame.time.Clock()
+	totalTime = 0
+
+	GRAVITY = 9.81
+
+	def drawPos(pos):
+		return (int(pos.x*SCALE), int((HEIGHT-pos.y)*SCALE))
+
+	def worldPos(pos):
+		return (pos.x/SCALE, HEIGHT - pos.y/SCALE)
+
+	def drawVector(v, start, scale=1, showBall=True, colour=PURPLE, width=2):
+		'''
+		Draws a vector, from
+			Vector v
+			Point start
+			float scale
+			bool showBall
+		'''
+		if v.mag > 0:
+			pygame.draw.line(DISPLAY, colour, drawPos(start), drawPos(start + (v/scale).endPos()), width)
+			if showBall:
+				pygame.draw.circle(DISPLAY, colour, drawPos(start+(v/scale).endPos()), 3, 3)
+
+	v1 = Vector(2, 3/2*math.pi)
+	v2 = Vector(3, 1/4*math.pi)
+
+	moveData = {
+		"lastPos": Point(),
+		"currentPos": Point(CENTRE.x, 0),
+		"lastScreenMousePos": Point(),
+		"mouseHeld": False
+	}
+
+	tickerPoints = []
+	MAX_TICKER_POINTS = 100
+
+	ball = Ball()
+	ball.setPos(Point(WIDTH/2, HEIGHT/2))
+	#ball.accelerate(Vector(10, math.pi/2))
+
+	stringLength = 2
+	useString = False
+
+	while True:
+		DISPLAY.fill(WHITE)
+		deltaT = CLOCK.get_time()/1000
+		totalTime += deltaT
+
+		moveData["lastPos"] = Point(moveData["currentPos"])
+		if not moveData["lastScreenMousePos"] == Point(pygame.mouse.get_pos()):
+			moveData["currentPos"] = Point(worldPos(Point(pygame.mouse.get_pos())))
+
+		moveData["lastScreenMousePos"] = Point(pygame.mouse.get_pos())
+
+		heldKeys = pygame.key.get_pressed()
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				pygame.quit()
+
+			elif e.type == pygame.KEYDOWN:
+				heldKeys = pygame.key.get_pressed()
+				if (heldKeys[pygame.K_RCTRL] or heldKeys[pygame.K_LCTRL]) and\
+					(heldKeys[pygame.K_w] or heldKeys[pygame.K_q]):
+					pygame.quit()
+				if (heldKeys[pygame.K_SPACE]):
+					mouseBallDist = moveData["currentPos"].distance(ball.pos)
+					if mouseBallDist > 0:
+						useString = not useString
+						stringLength = mouseBallDist
+
+			elif e.type == pygame.MOUSEBUTTONDOWN:
+				if e.button == 1:
+					moveData["mouseHeld"] = True
+
+			elif e.type == pygame.MOUSEBUTTONUP:
+				if e.button == 1:
+					moveData["mouseHeld"] = False
+
+		keyboardMoveSpeed = 1
+		if heldKeys[pygame.K_a]:
+			moveData["currentPos"] = moveData["currentPos"] - Point(keyboardMoveSpeed*deltaT, 0)
+		elif heldKeys[pygame.K_d]:
+			moveData["currentPos"] = moveData["currentPos"] + Point(keyboardMoveSpeed*deltaT, 0)
+		if heldKeys[pygame.K_w]:
+			moveData["currentPos"] = moveData["currentPos"] + Point(0, keyboardMoveSpeed*deltaT)
+		elif heldKeys[pygame.K_s]:
+			moveData["currentPos"] = moveData["currentPos"] - Point(0, keyboardMoveSpeed*deltaT)
+
+		if deltaT > 0:
+			mouseVelocity = Vector(((moveData["currentPos"]-moveData["lastPos"])/deltaT).pos())
+		else:
+			mouseVelocity = Vector()
+
+		if ball.bottom <= 0:
+			#if abs(ball.velocity.y) < 0.5:
+				#ball.setVelocity(Vector((ball.velocity.x, 0)))
+				#ball.setPos(Point(ball.pos.x, ball.radius))
+			if ball.velocity.y < 0:
+				ball.accelerate(Vector((0, -ball.velocity.y-ball.velocity.y*ball.cor)))
+		else:
+			ballMouseDistance = ball.pos.distance((moveData["currentPos"]))
+
+			ball.accelerate(Vector((0, -GRAVITY))*deltaT)
+
+			# for debug
+			tensionVector = Vector()
+			stringVector = Vector()
+			mouseAccelTension = Vector()
+			antiVelocityVector = Vector()
+			if ballMouseDistance >= stringLength and useString:
+				# String is taut
+				# We can work out angle between gravity and tension vector
+				# by creating a temporary vector for the string
+				stringVector = Vector((ball.pos-moveData["currentPos"]).pos())
+				# tension acts on the ball towards the string pivot:
+				oppositeStringAngle = stringVector.dir - math.pi
+
+				# calculate tension created by taut string against velocity
+				# away from string pivot
+				ballVelMag = ball.velocity.mag
+				ballVelAngle = ball.velocity.dir
+				theta = ballVelAngle - stringVector.dir 
+				antiVelocityMag = ballVelMag * math.cos(theta)
+				if antiVelocityMag > 0:
+					antiVelocityVector = Vector(antiVelocityMag, oppositeStringAngle)
+					#print(int(antiVelocityVector.dir/(math.pi*2)*360))
+					tensionVector = tensionVector + antiVelocityVector
+
+				# do tension created by movement
+				if mouseVelocity.mag > 0:
+					print(mouseVelocity.mag)
+				mouseAccelTensionMag = mouseVelocity.mag * math.cos(oppositeStringAngle - mouseVelocity.dir)
+				# this tension acts upon the ball along the oppositeStringAngle angle
+				mouseAccelTension = Vector(mouseAccelTensionMag, oppositeStringAngle)
+
+				tensionVector = tensionVector + mouseAccelTension
+
+				tensionVector.normalizeDir()
+				ball.accelerate(tensionVector)
+
+		if ball.left <= 0:
+			if ball.velocity.x < 0:
+				ball.accelerate(Vector((-ball.velocity.x-ball.velocity.x*ball.cor, 0)))
+
+		if ball.right >= WIDTH:
+			if ball.velocity.x > 0:
+				ball.accelerate(Vector((-ball.velocity.x-ball.velocity.x*ball.cor, 0)))
+
+		
+		if moveData["mouseHeld"]:
+			ball.setPos(moveData["currentPos"])
+			ball.setVelocity(mouseVelocity)
+		else:
+			ball.move(deltaT)
+
+			if ballMouseDistance >= stringLength and useString:
+				# test scale ball distance
+				distanceMod = stringLength/ballMouseDistance
+				distancePoint = Point(distanceMod*(ball.pos.x-moveData["currentPos"].x), distanceMod*(ball.pos.y-moveData["currentPos"].y))
+				ball.setPos(moveData["currentPos"] + distancePoint)
+
+		# draw ticker tape
+		if not moveData["mouseHeld"]:
+			tickerPoints.append(Point(ball.pos))
+			if len(tickerPoints) > MAX_TICKER_POINTS:
+				del tickerPoints[0]
+		else:
+			tickerPoints = []
+
+		for p in tickerPoints:
+			pygame.draw.circle(DISPLAY, BLUE, drawPos(p), 2, 2)
+
+		# draw mouse velocity vector
+		drawVector(mouseVelocity, moveData["currentPos"], 20, False, BLUE)
+
+		# velocity vector is scaled so it can be more easily comprehended
+		drawVector(ball.velocity, ball.pos, 10, False, GREEN, 1)
+
+		# draw ball
+		pygame.draw.circle(DISPLAY, RED, drawPos(ball.pos), int(ball.radius*SCALE), 1)
+
+		if useString:
+			pygame.draw.line(DISPLAY, GREEN, drawPos(ball.pos), drawPos(moveData["currentPos"]), 1)
+
+		# draw tension vector
+		drawVector(tensionVector, ball.pos, 10)
+
+		font = pygame.font.SysFont("monospace", 15)
+		# render text
+		ballVelLabel = font.render("Ball velocity: {}ms-1".format(int(ball.velocity.mag)), 1, BLACK)
+		DISPLAY.blit(ballVelLabel, (10, 10))
+		mouseVelLabel = font.render("Mouse velocity: {}ms-1".format(int(mouseVelocity.mag)), 1, BLACK)
+		DISPLAY.blit(mouseVelLabel, (10, 30))
+		#	pygame.draw.line(DISPLAY, PURPLE, drawPos(moveData["currentPos"]), drawPos(moveData["currentPos"] + stringVector.endPos()), 2)
+		#	pygame.draw.circle(DISPLAY, PURPLE, drawPos(moveData["currentPos"]+stringVector.endPos()), 3, 3)
+
+		pygame.display.update()
+		CLOCK.tick(40)
+
+if __name__ == "__main__":
+	main()
---
+++ objects.py
@@ -0,0 +1,243 @@
+import math
+
+class Vector:
+	def __init__(self, *args):
+		'''
+		Can be constructed from:
+			magnitude, direction
+			(x, y) [real coords]
+			Vector
+			[no parameters]
+		'''
+		constructed = False
+		if len(args) > 1:
+			if type(args[0]) in (int, float) and type(args[1]) in (int, float):
+				self.mag = args[0]
+				self.dir = args[1]
+				constructed = True
+		elif len(args) > 0:
+			if type(args[0]) == Vector:
+				v = args[0]
+				self.mag = v.mag
+				self.dir = v.dir
+				constructed = True
+			elif type(args[0]) == tuple:
+				x = args[0][0]
+				y = args[0][1]
+				self.mag = math.sqrt(x**2 + y**2)
+				if self.mag == 0:
+					self.dir = 0
+				else:
+					if y > 0:
+						self.dir = math.acos(x/self.mag)	# rearrangment of x() equation
+					elif x < 0:
+						self.dir = math.asin(-y/self.mag)+math.pi
+					else:
+						self.dir = math.asin(y/self.mag)	# rearrangment of x() equation
+					self.normalizeDir()
+				constructed = True
+		else:
+			self.mag = 0
+			self.dir = 0
+			constructed = True
+
+		if not constructed:
+			raise Exception("Invalid Vector constructor")
+
+	def __add__(self, vector):
+		return Vector((self.x + vector.x, self.y + vector.y))
+
+	def __sub__(self, vector):
+		return Vector((self.x - vector.x, self.y - vector.y))
+
+	def __mul__(self, mult):
+		if type(mult) == Vector:
+			return (self.x * mult.x) + (self.y * mult.y)
+		else:
+			return Vector((self.x * mult, self.y * mult))
+
+	def __rmul__(self, mult):
+		return self.__mul__(mult)
+
+	def __floordiv__(self, div):
+		if type(div) == Vector:
+			raise Exception("can't divide by vector yet")
+		else:
+			return Vector((self.x // div, self.y // div))
+
+	def __truediv__(self, div):
+		if type(div) == Vector:
+			raise Exception("can't divide by vector yet")
+		else:
+			return Vector((self.x / div, self.y / div))
+
+	def __pow__(self, ind):
+		toReturn = Vector(self)
+		for i in range(ind-1):
+			toReturn *= self
+		return toReturn
+
+	@property
+	def x(self):
+		return math.cos(self.dir) * self.mag
+
+	@property
+	def y(self):
+		return math.sin(self.dir) * self.mag
+
+	def flipY(self):
+		self.dir = 2*math.pi-self.dir
+		self.normalizeDir()
+
+	def drawEndPos(self):
+		return Point(self.x, -self.y)
+
+	def endPos(self):
+		return Point(self.x, self.y)
+
+	def rotate(self, angle):
+		self.dir += angle
+		# for the sake of readability, keep the angle between 0 <= a < 2pi
+		self.normalizeDir()
+
+	def normalizeDir(self):
+		self.dir = self.dir % (2*math.pi)
+
+	def unit(self):
+		mag = self.mag
+		mult = 1/mag
+		return self * mult
+
+	def __str__(self):
+		return "Vector(mag={}, dir={})".format(self.mag, self.dir)
+
+
+class Point:
+	def __init__(self, *args):
+		'''
+		Can be constructed from:
+			x, y
+			(x, y)
+			Point
+			[no parameters]
+		'''
+		constructed = False
+		if len(args) > 1:
+			if type(args[0]) in (int, float) and type(args[1]) in (int, float):
+				self.x = args[0]
+				self.y = args[1]
+				constructed = True
+		elif len(args) > 0:
+			if type(args[0]) == Point:
+				p = args[0]
+				self.x = p.x
+				self.y = p.y
+				constructed = True
+			elif type(args[0]) == tuple:
+				p = args[0]
+				self.x = p[0]
+				self.y = p[1]
+				constructed = True
+		else:
+			self.x = 0
+			self.y = 0
+			constructed = True
+
+		if not constructed:
+			raise Exception("Invalid Point constructor")
+
+	def translated(self, x, y):
+		return Point(self.x+x, self.y+y)
+
+	def __add__(self, p):
+		return Point(self.x+p.x, self.y+p.y)
+
+	def __sub__(self, p):
+		return Point(self.x-p.x, self.y-p.y)
+
+	def __mul__(self, m):
+		return Point(self.x*m, self.y*m)
+
+	def __floordiv__(self, div):
+		if type(div) == Point:
+			raise Exception("can't divide by point")
+		else:
+			return Point((self.x // div, self.y // div))
+
+	def __truediv__(self, div):
+		if type(div) == Vector:
+			raise Exception("can't divide by point")
+		else:
+			return Point((self.x / div, self.y / div))
+
+	def __eq__(self, p):
+		if self.x == p.x and self.y == p.y:
+			return True
+		return False
+
+	def __str__(self):
+		return "Point(x={}, y={})".format(self.x, self.y)
+
+	def distance(self, p):
+		return math.sqrt((self.x-p.x)**2 + (self.y-p.y)**2)
+
+	def pos(self):
+		return (self.x, self.y)
+
+	def int(self):
+		return (int(self.x), int(self.y))
+
+	def pygameToReal(self):
+		return Point(self.x, -self.y)
+
+	def realToPygame(self):
+		return Point(self.x, -self.y)
+
+
+class Ball:
+	def __init__(self):
+		self.velocity = Vector()
+		self.pos = Point()
+		self.mass = 0.1
+		self.radius = 0.1
+		self.cor = 0.8
+
+	def accelerate(self, a):
+		self.velocity += a
+
+	def applyForce(self, f):
+		# a = f/m
+		self.accelerate(f/self.mass)
+
+	def move(self, dt):
+		self.pos += self.velocity.endPos()*dt
+
+	def translate(self, v):
+		self.pos += v.endPos()
+
+	def setPos(self, p):
+		self.pos = p
+
+	def setVelocity(self, v):
+		self.velocity = v
+
+	def __eq__(self, p):
+		if self.x == p.x and self.y == p.y:
+			return True
+		return False
+
+	@property
+	def bottom(self):
+		return self.pos.y - self.radius
+
+	@property
+	def top(self):
+		return self.pos.y + self.radius
+
+	@property
+	def left(self):
+		return self.pos.x - self.radius
+
+	@property
+	def right(self):
+		return self.pos.x + self.radius
